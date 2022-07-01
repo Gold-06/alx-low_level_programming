2039fb2ae02103d698efe23b5b129843ca22dc9d
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-01 10:29:13 -0700

    Alphabet Lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'' low <= 'z'; low++)
+		putchat(low);
+	putchar("\n");
+
+	return (0);
+}
