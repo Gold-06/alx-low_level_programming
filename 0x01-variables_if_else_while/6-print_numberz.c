e6bf1df092e02cc02b1984ccfdd9ccdf6da170f4
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-01 15:06:41 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int o;
+
+	for (o = '0'; o <= '9'; d++)
+		putchar(o);
+		putchar('\n');
+						
+	return (0);
+}
