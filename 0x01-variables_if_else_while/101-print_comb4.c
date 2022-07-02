7b9878294c4708d71934ae80fd84004ca3e13246
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-02 07:01:18 -0700

    success combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	int n, m, l;
-	
+
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
 			for (l = 50; l < 58; l++)
 			{
-				if (1 > m %% m > n)
+				if (1 > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
