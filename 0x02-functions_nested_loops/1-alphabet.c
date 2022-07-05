589add1e5d69d4889e6c12c4d28c02b9063d709a
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-05 01:54:48 -0700

    Lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	int b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+	return (0);
+}
