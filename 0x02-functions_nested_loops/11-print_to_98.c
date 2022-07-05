523e6eb26315b3cc84f7a42bb4bae07f1c2aeb2d
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-05 09:52:45 -0700

    98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,40 @@
-#include
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - a functions that print natural number from n to 98
+ * user inputs number prints to 98
+ * @n: number input
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if  (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
