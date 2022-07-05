0ed4b3732246ec5bfa26be27f1dee96986eb50be
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-05 08:48:48 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,25 @@
-#include <stdio.h>
+#include"main.h"
+/**
+ * jack_bauer - a function that print every minute of the day
+ * h = hour, m = minutes
+ * / 10 allows second digit to rotate
+ * for loop break before passing 24:00
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
