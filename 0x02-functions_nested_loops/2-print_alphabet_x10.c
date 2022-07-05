103d69d4ccdabd56b2d6756671c8314c6dd4c399
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-05 02:06:24 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
-#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int b, mn;
+	mn = 0;
+
+	while (mn < 10)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		mn++;
+		_putchar('\n');
+	}
+	return (0);
+}
