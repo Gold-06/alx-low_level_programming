5b714492f4ab672a0c39b9fa24357c7fa24d6a8a
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-07 07:22:07 -0700

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * mul - a function that multiplies two integer
- * @a - first input
- * @b - second input
- * Return: multiplication result
+ * @a:  first input
+ * @b:  second input
+ * Return: multiplication result of the 2 inputs
  */
 int mul(int a, int b)
 {
