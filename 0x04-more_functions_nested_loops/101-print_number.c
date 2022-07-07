9d764353653490b34a45e418155e5cb7e81dfeb7
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-07 12:07:26 -0700

    Integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_number - a function that print an integer
- * #n: input integer
+ * print_number - a function that prints an integer
+ * @n: print int
  * Return: inputted integer
  */
 void print_number(int n)
@@ -26,7 +26,7 @@ void print_number(int n)
 		d /= 10;
 		count *= 10;
 	}
-	for (count >= 1; count /= 10;)
+	for (; count >= 1; count /= 10;)
 	{
 		_putchar(((m / count) % 10) + 48);
 	}
