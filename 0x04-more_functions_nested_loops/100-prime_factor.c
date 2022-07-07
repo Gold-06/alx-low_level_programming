2af5cef8484a2e6b998b600d8e273e77e9c7618d
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-07 10:15:48 -0700

    Prime Factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long n, i;
+
+	n = 612852475143;
+	for (i = 2; i < n; i++)
+	{
+		while (n % i == 0)
+			n = n / i;
+	}
+	printf("%lu \n", n);
+	return (0);
+}
