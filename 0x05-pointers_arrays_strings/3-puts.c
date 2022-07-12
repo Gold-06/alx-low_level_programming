d58d16789af425fa3d60ea1400b6cac3cc46e1ae
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-12 06:57:30 -0700

    String

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - a function that print a string
+ * @str: string input
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int l;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+		_putchar(str[l]);
+	}
+	putchar('\n');
+}
