14f65c568a25be918b1e49c5b12c8c3228544765
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-14 07:41:45 -0700

    Compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strcmp - a function that compares two strings
- * @si: first input
+ * @s1: first input
  * @s2: second input
  * Return: Always 0
  */
@@ -9,7 +9,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 00; s1[i] != '\0'; && s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; && s2[i] != '\0'; i++)
 	{
 		if  (s1[i] != s2[i])
 		{
