875fe923ff641f459a9a2434f5ce5e50c1f365d2
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-14 02:52:52 -0700

    Reverse content

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverse the content of an array integer
+ * @a: content
+ * @n: element of content
+ * Return: Always 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
+	return (0);
+}
