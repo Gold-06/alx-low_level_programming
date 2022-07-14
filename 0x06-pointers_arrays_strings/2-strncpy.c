e6d2e40724c765974678ff9b3b88d52756017504
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-14 07:38:22 -0700

    Copies Strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,9 +2,9 @@
 #include <string.h>
 /**
  * _strncpy - a function that copies strings
- * #dest: copy to
+ * @dest: copy to
  * @src: copy from
- * #n: input numberof char
+ * @n: input numberof char
  * Return: Always 0
  */
 char *_strncpy(char *dest, char *src, int n)
