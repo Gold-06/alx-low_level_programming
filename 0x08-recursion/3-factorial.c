4348de11ba41e79883b2bb35a15f2e000951aba5
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-20 00:34:45 -0700

    Print factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - a function that return the factorial of a given number
+ * @n: input
+ * Return: ALways 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
