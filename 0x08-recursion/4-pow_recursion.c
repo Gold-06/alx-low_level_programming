3cf053175176459c9eedf9d7d77e02b5a2ac53ef
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-20 00:48:38 -0700

    Raised power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y
+ * _pow_recursion - a function that returns the value of x
+ * raised to the power of y
  * @x: first input
  * @y: second input
  * Return: Always 0
