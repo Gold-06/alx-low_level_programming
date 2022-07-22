90bcb395c5fb2b3bc0aaf3c26c0aa95823c97b93
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-22 12:09:20 -0700

    Islower

---
+++ 0x09-static_libraries/cfiles/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *  * _islower - a function that checks for lower case character
+ *
+ *   * @c: single letter input
+ *
+ *    * Return: ways 0 (Success)
+ *
+ *     */
+
+int _islower(int c)
+
+{
+
+		if (c >= 'a' && c <= 'z')
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
