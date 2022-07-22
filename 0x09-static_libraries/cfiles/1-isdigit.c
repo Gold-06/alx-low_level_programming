7400010c51b96306229a7382411bb04e1ddf5213
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-22 11:27:12 -0700

    Isdigit

---
+++ 0x09-static_libraries/cfiles/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isdigit - a function that checks for a digit
+ *
+ *   * @c: input
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int _isdigit(int c)
+
+{
+
+		if ((c >= 48) && (c <= 57))
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
