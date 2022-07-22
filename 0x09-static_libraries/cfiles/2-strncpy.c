c29d2994919d64beebc89100b2d63e7ae6651b39
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-22 11:46:21 -0700

    Copies string

---
+++ 0x09-static_libraries/cfiles/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+ *
+ *  * _strncpy - a function that copies strings
+ *
+ *   * @dest: copy to
+ *
+ *    * @src: copy from
+ *
+ *     * @n: input numberof char
+ *
+ *      * Return: Always 0
+ *
+ *       */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+		strncpy(dest, src, n);
+
+			return (dest);
+
+}
