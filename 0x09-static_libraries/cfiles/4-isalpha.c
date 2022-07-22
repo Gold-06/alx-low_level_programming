c0df71a7f5d8da76eb5b3172166d1a90d95ac491
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-22 12:11:25 -0700

    Isalpha

---
+++ 0x09-static_libraries/cfiles/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *  * _isalpha - a functions that checks for alphabetic character
+ *
+ *   * @c: single letter input
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int _isalpha(int c)
+
+{
+
+		if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+
+					return (1);
+
+			else
+
+						return (0);
+
+}
