1cfc43faef2ceb890f160759181f6a75923c2a4c
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-22 12:13:56 -0700

    Absolute value

---
+++ 0x09-static_libraries/cfiles/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ *  * _abs - a functions that print the absolute value of an integer
+ *
+ *   * @ab: integer input
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int _abs(int ab)
+
+{
+
+		return (ab * ((ab > 0) - (ab < 0)));
+
+}
