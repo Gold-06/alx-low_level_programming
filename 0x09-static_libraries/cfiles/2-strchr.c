a5899271ab781e8746ec65d85ffbf78a2b180e3f
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-22 11:54:29 -0700

    Locate a character

---
+++ 0x09-static_libraries/cfiles/2-strchr.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ *
+ *  * _strchr - a function that locate character in string
+ *
+ *   * @s: first input
+ *
+ *    * @c: second input
+ *
+ *     * Return: Always 0
+ *
+ *      */
+
+char *_strchr(char *s, char c)
+
+{
+
+		int d;
+
+
+
+			for (d = 0; s[d] >= '\0'; d++)
+
+					{
+
+								if (s[d] == c)
+
+												return (s + d);
+
+									}
+
+				return (NULL);
+
+}
