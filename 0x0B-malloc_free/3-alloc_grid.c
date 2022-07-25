b0216578b87ad322b87445292ad53bf381c4ad6b
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-25 07:02:45 -0700

    Print grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - a function that return pointer
+ * @width: input
+ * @height: input
+ * Return: 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int **iarray;
+	int i, n;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	iarray = malloc(sizeof(int) * height);
+
+	if (iarray == NULL)
+	{
+		free(iarray);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		iarray[i] = malloc(sizeof(int) * width);
+		if (iarray == NULL)
+		{
+			free(iarray);
+			return (null);
+		}
+		for (n = 0; n < width; n++)
+		{
+			iarray[i][n] = 0;
+		}
+	}
+	return (iarray);
+}
