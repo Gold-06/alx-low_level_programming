70d5a9bbb35b96878ee11faea7dcbda55cf99f20
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-25 07:17:08 -0700

    Free a dimensional

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - a function that frees 2 dimensional
+ * @grid: array to free
+ * @height: input
+ * Return: 0
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
