351c5c76ee66d80017c487a13c636f259d8d1c17
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-07-28 14:34:55 -0700

    Concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,11 +20,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1[size1] != '\0')
 	{
-		size1++
+		size1++;
 	}
 	while (s2[size2] != '\0')
 	{
-		size2++
+		size2++;
 	}
 	if (n > size2)
 		n = size2;
