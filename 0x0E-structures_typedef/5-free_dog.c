e41bd9e8b40ac6addd13b33c91217669df5bd577
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-01 13:55:00 -0700

    Frees dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,12 +4,14 @@
 /**
  * free_dog - a function that frees dog
  * @d: struct to free
+ * Return: 0
  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
+
 	free(d->owner);
-	free(d->name
+	free(d->name);
 	free(d);
 }
