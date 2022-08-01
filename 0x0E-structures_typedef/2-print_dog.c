f9000364a6cf6088427f580bc6a01a412ff07716
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-01 12:17:09 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog - a function that print a struct drug
+ * @d: struct name
+ * Return: 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nill)";
+	if (d->age <= 0)
+		printf("Age: (nil)\n");
+	if (d->owner == NULL)
+		d->owner = "(nill)";
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	printf(owner: %s\n", d->owner);
+}
