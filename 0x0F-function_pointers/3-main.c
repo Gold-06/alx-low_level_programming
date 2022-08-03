3753cbd587beae2082cc37a4bdd3036d50259127
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-03 15:40:33 -0700

    Simple Operation

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - Entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+			exit(99);
+	}
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	printf("%d\n", operation(i, j));
+	return (0);
+}
