c53cd6cc9102c3bad7882302e4f8235730d71ca9
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-11 07:16:50 -0700

    Elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_list - a function that print all elements
+ * @h: structure
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
