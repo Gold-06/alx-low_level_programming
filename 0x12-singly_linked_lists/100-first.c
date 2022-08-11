fc4cf061ac1a9840575b1ff0835747dcd731da1d
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-11 12:41:21 -0700

    Printf

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n")
+}
