4005c59017439a5e97f223067c5591c5ee18006b
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-16 15:33:23 -0700

    Print

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * print_listint_safe - a function that print listint_t
+ * @head: head
+ * Return: 0
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	if (head == NULL)
+		exit(98);
+
+	return (0);
+}
