c5ac236f1563a606563eff98fcd32cda022307ab
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-16 15:02:53 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,5 +31,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = *head;
 		h->next = new;
 	}
+	else
+	{
+		new->next = h->next;
+		h->next = new;
+	}
 	return (new);
 }
