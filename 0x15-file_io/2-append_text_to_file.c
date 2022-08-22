4ad37905e0bd9bd51a0bb085148f67752df31978
Ayodele Tobiloba <oluwatobilobaayodele94@gmail.com>
2022-08-22 10:37:43 -0700

    Append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,10 +9,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, wr, i = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 
